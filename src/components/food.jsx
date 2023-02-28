295acf7940dc918d3cb22dcd388058b442bf845e
subhash13 <msubhash138@gmail.com>
2023-02-28 14:40:15 +0530

    single post done

--- src/components/food.jsx
+++ src/components/food.jsx
@@ -1,23 +1,22 @@
 import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Banner from "./banner";
 import axios from "axios";
 const Food = () => {
     let title = "All Posts"
-    let navigate = useNavigate()
     let [posts, setPosts] = useState([])
     useEffect(() => {
         let fetchData = async () => {
             const res = await axios.get("http://localhost:4000/posts")
-            const data = res.data
+            const data = await res.data
             setPosts(data)
         }
         fetchData()
     }, [])
 
-    const readPost = (id) =>{
-        navigate(`/posts/${id}`)
-    }
+    // const readPost = (id) => {
+    //     navigate(`/posts/${id}`)
+    // }
     return (
         <div className="food">
             <Banner data={title} />
@@ -25,16 +24,17 @@ const Food = () => {
                 {posts.map((post) => {
                     return (
                         <div className="post mx-3" key={post._id}>
-                            <img src={post.image} className="rounded-2 homePostImage" width="300" height="350" alt="" />
+                            <Link to={`/food/${post._id}`}><img src={post.image} className="rounded-2 homePostImage" width="300" height="350" alt="" /></Link>
                             <div className="info mt-3">
                                 <h2 style={{ fontFamily: "inherit" }}>{post.title}</h2>
                                 <p className="text-secondary">- Written by {post.author}</p>
-                                <button onClick={()=>readPost(post._id)} className="btn btn-dark rounded-1 homeBtn">Read more &gt;</button>
+                                <Link to={`/food/${post._id}`} className="btn btn-dark rounded-1 homeBtn">Read more &gt;</Link>
                             </div>
                         </div>
                     )
                 })}
             </div>
+
         </div>
     );
 }
--- src/components/singlePost.jsx
+++ src/components/singlePost.jsx
@@ -1,25 +1,50 @@
 import Banner from "./banner";
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
+import { useParams, Link } from "react-router-dom";
 import axios from "axios";
 
-const SinglePost = ({_id}) => {
+const SinglePost = () => {
     const title = "Single Post";
-    const [post,setPosts] = useState({})
+    const [post, setPosts] = useState({})
+    let params = useParams()
 
-    useEffect(()=>{
+    useEffect(() => {
         let fetchData = async () => {
-            const res = await axios.get(`http://localhost:4000/posts/${_id}`)
-            const data = res.data
+            const res = await axios.get(`http://localhost:4000/posts/${params.id}`)
+            const data = await res.data
             setPosts(data)
+            console.log(post);
         }
         fetchData()
-    },[_id])
-    return ( 
+    }, [post])
+    return (
         <div className="singlepost">
             <Banner data={title} />
-            <h1>{post.title}</h1>
+            <div className="blogInfo text-center my-5 d-flex justify-content-center">
+                <div className="image col-5" >
+                    <img src={post.image} className="rounded-2" alt="" height="900" width="700" />
+                </div>
+                <div className="info col-5 text-start">
+                    <h1 className="my-3">{post.title}</h1>
+                    <div className="line mb-3"></div>
+                    <p>{post.summary}</p>
+                    <div className="location">
+                        <h2> <img width="30" src="/images/location.svg" alt="" /> Location</h2>
+                        <div className="line mb-3"></div>
+                        <iframe src={post.location}
+                            className="my-3"
+                            width="600" height="450"
+                            allowfullscreen=""
+                            loading="lazy"
+                            referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
+                </div>
+            </div>
+            <div className="linkBtn text-center">
+                <Link to='/food' className="btn btn-dark btn-lg rounded-1 my-4 homeBtn">&lt; Go back to Posts</Link>
+            </div>
         </div>
-     );
+    );
 }
- 
+
 export default SinglePost;
